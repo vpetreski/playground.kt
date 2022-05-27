8620fbe3132d7ba59e46ae6d16e0699bdb754100
Vanja <vanja@petreski.co>
2022-05-26 20:12:54 -0500

    OperationsQuiz

--- src/main/kotlin/functional/OperationsQuiz.kt
+++ src/main/kotlin/functional/OperationsQuiz.kt
@@ -41,6 +41,42 @@ fun main() {
     // true
     println(heroes.any { it.gender == Gender.FEMALE })
 
-    //
-    println()
+    // 29
+    val mapByAge: Map<Int, List<Hero>> = heroes.groupBy { it.age }
+    val (age, group) = mapByAge.maxByOrNull { (_, group) -> group.size }!!
+    println(age)
+
+    // 42
+    val mapByName: Map<String, Hero> = heroes.associateBy { it.name }
+    println(mapByName["Frenchy"]?.age)
+    // NoSuchElementException if no element!
+    println(mapByName.getValue("Frenchy").age)
+
+    // 0
+    val mapAgesByName = heroes.associate { it.name to it.age }
+    println(mapAgesByName.getOrElse("UnknownName") { 0 })
+
+    // The Captain
+    // Example of bad written code hard to read - DO NOT DO THIS
+    val (frst, scnd) = heroes
+        .flatMap { heroes.map { hero -> it to hero } } // it is from flatMap, hero is from heroes
+        .maxByOrNull { it.first.age - it.second.age }!! // it here is from maxByOrNull which refers to a Pair of heroes
+    // So for each it from flatMap, let's call it foo, to illustrate, we will get a Pair with each hero
+    println(heroes.map { hero -> "foo" to hero })
+    // So we will have a list of lists - each combination of heroes
+    println(heroes.flatMap { heroes.map { hero -> it to hero } })
+    // Since The Captain is the oldest, difference with him and the youngest will win
+    println(frst.name)
+
+    // The Captain
+    // It's better to use explicit lambda parameters (no it), extracting intermediate variables and proper naming, for clarity in these situations
+    val allPossiblePairs = heroes
+        .flatMap { first -> heroes.map { second -> first to second } }
+    val (theOldest, theYoungest) = allPossiblePairs
+        .maxByOrNull { it.first.age - it.second.age }!!
+    println(theOldest.name)
+
+    // The Captain
+    // But if we think what that code is doing, we actually really need this
+    println(heroes.maxByOrNull { it.age }?.name)
 }
